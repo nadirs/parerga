066d0e800f571f9ddc83c74a2fc2363c50055e92
nadirs <nadirsampaoli@gmail.com>
2012-11-13 20:08:15 +0100

    Fix indentation errors

--- diy/application.py
+++ diy/application.py
@@ -2,10 +2,10 @@ from flask import Flask
 import platform
 application = Flask(__name__)
 
- @application.route("/")
- def index():
-         return 'Hello from Flask'
+@application.route("/")
+def index():
+    return 'Hello from Flask'
 
-     @application.route("/info")
-     def info():
-             return platform.python_version()
+@application.route("/info")
+def info():
+    return platform.python_version()
