d0cd55cb921e94533f65564bbfdd2635a06c8921
nadirs <nadirsampaoli@gmail.com>
2012-12-29 22:16:38 +0100

    Remove useless table from sql schema

--- diy/schema.sql
+++ diy/schema.sql
@@ -4,12 +4,3 @@ create table entries (
     date_added integer not null,
     path text not null
 );
-
-drop table if exists comments;
-create table comments (
-    id integer primary key autoincrement,
-    entry_id integer not null,
-    author text not null default 'anonymous',
-    content text not null,
-    foreign key(entry_id) references entries(id)
-);
