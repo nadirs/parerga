e93a321e3d137fb21a42d0e0bfd257a537be05d3
nadirs <nadirsampaoli@gmail.com>
2013-01-15 20:28:47 +0100

    Update path vars for the new source location

--- diy/parerga/config.py
+++ diy/parerga/config.py
@@ -3,7 +3,7 @@
 import os
 
 # directories constants
-PARERGA_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARERGA_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PARERGA_ENTRY_DIR = os.path.join(PARERGA_ROOT_DIR, "p")
 PARERGA_STATIC_DIR = os.path.join(PARERGA_ROOT_DIR, "static")
 PARERGA_TEMPLATE_DIR = os.path.join(PARERGA_ROOT_DIR, "templates")
